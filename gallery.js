41416ba4b5f5f783c8e797f4948c042c3588c007
Alvaro Gracia <alvaro.gracia.reg@gmail.com>
2023-04-23 10:20:41 +0200

    new gallery

--- gallery.js
+++ gallery.js
@@ -1,32 +1,26 @@
-var gallery = document.querySelector('#gallery');
-var getVal = function (elem, style) { return parseInt(window.getComputedStyle(elem).getPropertyValue(style)); };
-var getHeight = function (item) { return item.querySelector('.content').getBoundingClientRect().height; };
-var resizeAll = function () {
-    var altura = getVal(gallery, 'grid-auto-rows');
-    var gap = getVal(gallery, 'grid-row-gap');
-    gallery.querySelectorAll('.gallery-item').forEach(function (item) {
-        var el = item;
-        el.style.gridRowEnd = "span " + Math.ceil((getHeight(item) + gap) / (altura + gap));
+let photosToShow = ["res/fotos/baño1.jpg","res/fotos/cocina1.jpg","res/fotos/cocina2.jpg","res/fotos/comedor1.jpg","res/fotos/comedor2.jpg","res/fotos/comedor3.jpg","res/fotos/comedor4.jpg","res/fotos/dormitorio1.jpg","res/fotos/dormitorio2.jpg","res/fotos/dormitorio3.jpg","res/fotos/dormitorio4.jpg","res/fotos/dormitorio5.jpg","res/fotos/porche1.jpg"]
+
+/*MAIN GALLERY*/
+const showPhoto = (src) => {
+    console.log(src)
+    document.getElementById('mainphoto').src = src;
+}
+
+showPhoto(photosToShow[0]);
+
+/*SUBGALLERY*/
+const showSubGallery = () => {
+    const subframe = document.getElementById('subframe')
+
+    photosToShow.forEach(element => {
+        let newImg = document.createElement('img')
+        newImg.classList.add('subgalleryimg')
+        newImg.src = element
+        newImg.onclick = function() {showPhoto(element)}
+        subframe.appendChild(newImg)
+
+        console.log(newImg)
     });
-};
-gallery.querySelectorAll('img').forEach(function (item) {
-    item.classList.add('byebye');
-    if (item.complete) {
-        console.log(item.src);
-    }
-    else {
-        item.addEventListener('load', function () {
-            var altura = getVal(gallery, 'grid-auto-rows');
-            var gap = getVal(gallery, 'grid-row-gap');
-            var gitem = item.parentElement.parentElement;
-            gitem.style.gridRowEnd = "span " + Math.ceil((getHeight(gitem) + gap) / (altura + gap));
-            item.classList.remove('byebye');
-        });
-    }
-});
-window.addEventListener('resize', resizeAll);
-gallery.querySelectorAll('.gallery-item').forEach(function (item) {
-    item.addEventListener('click', function () {        
-        item.classList.toggle('full');        
-    });
-});
+}
+
+showSubGallery();
